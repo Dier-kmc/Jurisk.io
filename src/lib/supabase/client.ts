5b48be0e234a088c907c2498d891a7abb39dde00
Dierdi001 <komaclodidier@gmail.com>
2026-02-11 15:12:42 +0000

    create bucket for file storage and service role refacto

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,14 +1,11 @@
 // src/lib/supabase/client.ts
 import { createClient } from '@supabase/supabase-js'
 
-// URL publique du projet Supabase
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
-// Clé côté serveur (SERVICE ROLE) pour upload privé
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
 
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error('Supabase environment variables are not set')
 }
 
-// Créer le client Supabase
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
